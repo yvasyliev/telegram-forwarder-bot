4c2804dc936a91e655695213214ca000514f7868
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-03-25 12:02:16 +0200

    refactor: use ISO_INSTANT formatter in EditedDeserializer constructor

--- telegram-forwarder-starter-reddit/src/main/java/io/github/yvasyliev/forwarder/telegram/reddit/deser/EditedDeserializer.java
+++ telegram-forwarder-starter-reddit/src/main/java/io/github/yvasyliev/forwarder/telegram/reddit/deser/EditedDeserializer.java
@@ -5,14 +5,15 @@ import tools.jackson.databind.DeserializationContext;
 import tools.jackson.databind.ext.javatime.deser.InstantDeserializer;
 
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Deserializer for the "edited" field in Reddit API responses. It returns an {@link Instant} if the value is a number
- * (indicating a timestamp), or null if the value is {@code false} (indicating that the post was not edited).
+ * (indicating a timestamp), or {@code null} if the value is {@code false} (indicating that the post was not edited).
  */
 public class EditedDeserializer extends InstantDeserializer<Instant> {
     public EditedDeserializer() {
-        super(InstantDeserializer.INSTANT, (Boolean) null);
+        super(InstantDeserializer.INSTANT, DateTimeFormatter.ISO_INSTANT);
     }
 
     @Override
