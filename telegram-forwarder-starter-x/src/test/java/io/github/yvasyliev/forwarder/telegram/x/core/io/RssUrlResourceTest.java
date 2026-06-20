abc9057b7b99931c7cbc734fe7bec734de262fd5
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2026-06-20 19:07:27 +0300

    bugfix: update `RssUrlResourceTest` to handle user agent and connection setup

--- telegram-forwarder-starter-x/src/test/java/io/github/yvasyliev/forwarder/telegram/x/core/io/RssUrlResourceTest.java
+++ telegram-forwarder-starter-x/src/test/java/io/github/yvasyliev/forwarder/telegram/x/core/io/RssUrlResourceTest.java
@@ -6,18 +6,22 @@ import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class RssUrlResourceTest {
     @Test
     void testCustomizeConnection() {
         var userAgent = "Test User Agent";
         var conn = mock(HttpURLConnection.class);
-        var resource = new RssUrlResource(mock(), userAgent, null);
+        var resource = new RssUrlResource(mock(), userAgent, Set.of());
 
         assertDoesNotThrow(() -> resource.customizeConnection(conn));
 
@@ -26,7 +30,11 @@ class RssUrlResourceTest {
 
     @Test
     void testGetInputStream() throws IOException {
-        var resource = new RssUrlResource(mock(), null, null);
+        var url = mock(URL.class);
+        var urlConnection = mock(URLConnection.class);
+        var resource = new RssUrlResource(url, null, Set.of());
+
+        when(url.openConnection()).thenReturn(urlConnection);
 
         assertThat(resource.getInputStream()).isInstanceOf(ByteFilteringInputStream.class);
     }
